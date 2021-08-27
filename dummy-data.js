8ae9555dda21623a05e378dcf51d5e5e3447c520
Nigel <75135800+nigels-code@users.noreply.github.com>
2021-08-27 18:24:18 +0100

    add AllEvents page and styles

--- dummy-data.js
+++ dummy-data.js
@@ -13,7 +13,7 @@ const DUMMY_EVENTS = [
 		id: 'e2',
 		title: 'Networking for introverts',
 		description:
-			"We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
+			"We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promise!",
 		location: 'New Wall Street 5, 98765 New Work',
 		date: '2021-05-30',
 		image: 'images/introvert-event.jpg',
