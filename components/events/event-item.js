941c533bd65d5b114f165fc1f87fec6788b9ac03
Nigel <75135800+nigels-code@users.noreply.github.com>
2021-08-27 15:56:35 +0100

    add event details page and styles

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,8 @@
-import Link from 'next/link';
 import styles from './event-item.module.css';
+import Button from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 
 function EventItem(props) {
 	const { title, image, date, location, id } = props;
@@ -18,14 +21,21 @@ function EventItem(props) {
 				<div className={styles.summary}>
 					<h2>{title}</h2>
 					<div className={styles.date}>
+						<DateIcon />
 						<time>{prettyDate}</time>
 					</div>
 					<div className={styles.address}>
+						<AddressIcon />
 						<address>{prettyAddress}</address>
 					</div>
 				</div>
 				<div className={styles.actions}>
-					<Link href={exploreLink}>Explore Event</Link>
+					<Button link={exploreLink}>
+						<span>Explore Event</span>
+						<span className={styles.icon}>
+							<ArrowRightIcon />
+						</span>
+					</Button>
 				</div>
 			</div>
 		</li>
